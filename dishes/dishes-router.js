24701431eed9b87ba0d4a758fe7f093a4ea93239
Seth Lachman <lachmanseth17@gmail.com>
2019-06-06 23:34:53 -0400

    Added post dish method and get dish by id method to router.

--- dishes/dishes-router.js
+++ dishes/dishes-router.js
@@ -2,10 +2,32 @@ const router = require("express").Router();
 
 const Dishes = require("./dishes-model.js");
 
+router.post("/", (req, res) => {
+  const newDish = req.body;
+  Dishes.addDish(newDish)
+    .then(dish => {
+      res.status(201).json(dish);
+    })
+    .catch(err => {
+      res.status(500).json({ message: err });
+    });
+});
+
 router.get("/", (req, res) => {
   Dishes.getDishes()
     .then(dishes => {
       res.status(200).json(dishes);
+    })
+    .catch(err => {
+      res.status(500).json({ message: err });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  Dishes.getDish(id)
+    .then(dish => {
+      res.status(200).json(dish);
     })
     .catch(err => {
       res.status(500).json({ message: err });
